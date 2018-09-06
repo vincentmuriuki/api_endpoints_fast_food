c9418b8622c3513be017d3c04071628f7c7adea2
Vincent <vincentmuriuki42@gmail.com>
2018-09-06 07:49:41 +0300

    Added GET endpoint to fetch a specific order

--- fast_food/api_endpoints.py
+++ fast_food/api_endpoints.py
@@ -1,8 +1,10 @@
-# 
+# API endpoints
 
 import json
 from flask import session, abort, request
 from fast_food import app
+from fast_food import models
+from fast_food.models import orders
 
 
 @app.route('/orders', methods=['POST'])
@@ -21,3 +23,14 @@ def all_orders ():
     session.clear()
     session['order_list'] = models.orders
     return json.dumps(session['order_list'])
+
+# API endpoint to fetch a specific order
+@app.route('/orders/<int:order_id>', methods = ['GET'])
+def specific_order(order_id):
+    session.clear()
+    session['order_list'] = models.orders
+    for j in session['order_list']:
+        order = j.get(str('id'))
+        if order == order_id:
+            return json.dumps(j)
+        abort(404)
